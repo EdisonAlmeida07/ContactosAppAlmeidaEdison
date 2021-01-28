1781c6782018c69496974ffc31fccf601388246f
Edison Almeida <eddy_son07@hotmail.es>
2021-01-27 19:37:56 -0500

    Commit de Edison 1 Laboratorio2 con el laboratorio 3

---
+++ app/src/main/java/com/example/contactosapp/Constantes.kt
@@ -0,0 +1,5 @@
+package com.example.contactosapp
+
+const val MIN_PASSWORD_LENGTH = 8
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
--- app/src/main/java/com/example/contactosapp/LoginActivity.kt
+++ app/src/main/java/com/example/contactosapp/LoginActivity.kt
@@ -1,11 +1,73 @@
 package com.example.contactosapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Patterns
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var buttonLogin : Button
+    lateinit var editTextTextEmailAddress : EditText
+    lateinit var editTextTextPassword : EditText
+    lateinit var checkBoxRecordarme :CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+    // Inicializacion de variables
+
+        buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        editTextTextEmailAddress = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        checkBoxRecordarme = findViewById<CheckBox>(R.id.checkBoxRecordarme)
+        //Lectura de valores
+
+        buttonLogin.setOnClickListener {
+            if (!ValidarDatos())
+                return@setOnClickListener
+
+            if(checkBoxRecordarme.isChecked){
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,editTextTextEmailAddress.text.toString())
+                editor.putString(PASSWORD_KEY,editTextTextPassword.text.toString())
+                editor.commit()
+            }
+            else{
+                val editor = sharedPref.edit()
+                editor.putString(LOGIN_KEY,"")
+                editor.putString(PASSWORD_KEY,"")
+                editor.commit()
+            }
+        }
+    }
+    fun ValidarDatos(): Boolean {
+        fun CharSequence?.isValidEmail() =
+                !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+        if (editTextTextEmailAddress.text.isNullOrEmpty()) {
+            editTextTextEmailAddress.setError(getString(R.string.editTextTextEmailAddress_hint))
+            editTextTextEmailAddress.requestFocus()
+            return false
+        }
+        if (!editTextTextEmailAddress.text.isValidEmail()) {
+            editTextTextEmailAddress.setError(getString(R.string.email_NoValido))
+            editTextTextEmailAddress.requestFocus()
+            return false
+        }
+        if (editTextTextPassword.text.isNullOrEmpty()) {
+            editTextTextPassword.setError(getString(R.string.editTextPassword_hint))
+            editTextTextPassword.requestFocus()
+            return false
+        }
+        if (editTextTextPassword.text.length < MIN_PASSWORD_LENGTH) {
+            editTextTextPassword.setError(getString(R.string.password_longitudNoValida))
+            editTextTextPassword.requestFocus()
+            return false
+        }
+        return true
     }
 }
